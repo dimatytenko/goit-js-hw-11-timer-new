6dd4a1096ae89d06dc55ef41c5d11fcfcde6bf80
Dima Tytenko <amgpetronass@gmail.com>
2021-10-05 21:37:42 +0300

    add

--- index.js
+++ index.js
@@ -2,33 +2,3 @@ new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
 });
-
-<div class="timer" id="timer-1">
-  <div class="field">
-    <span class="value" data-value="days">
-      11
-    </span>
-    <span class="label">Days</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="hours">
-      11
-    </span>
-    <span class="label">Hours</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="mins">
-      11
-    </span>
-    <span class="label">Minutes</span>
-  </div>
-
-  <div class="field">
-    <span class="value" data-value="secs">
-      11
-    </span>
-    <span class="label">Seconds</span>
-  </div>
-</div>;
